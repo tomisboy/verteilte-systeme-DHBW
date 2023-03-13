084b83d25d1393575b90302f9af2a29143241b41
Thomas Alpert <thomasalpert@gmail.com>
2023-03-13 21:40:07 +0100

    HA, replikation, persistent data

--- testclient/test/test/Program.cs
+++ testclient/test/test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Cassandra;
+using Newtonsoft.Json;
 
 class Program
 {
@@ -11,24 +12,39 @@ class Program
         
         // Cassandra Cluster und Session erstellen
         // Cluster cluster = Cluster.Builder().AddContactPoint("localhost").Build();
-        // Cluster cluster = Cluster.Builder().AddContactPoint("193.196.55.166").Build();
-        Cluster cluster = Cluster.Builder().AddContactPoint("cassandra").Build();
+        Cluster cluster = Cluster.Builder().AddContactPoint("193.196.55.166").Build();
+        //Cluster cluster = Cluster.Builder().AddContactPoint("cassandra").Build();
         ISession session = cluster.Connect("vs2");
 
         // Insert-Statement vorbereiten
         var insertStatement = session.Prepare("INSERT INTO test (name, password) VALUES (?, ?)");
 
         // 100 verschiedene Datensätze als SQL-Statements hinzufügen
-        for (int i = 1; i <= 10;  i++)
+        for (int i = 1; i < 999;  i++)
         {
             // Parameterwerte setzen
-            var statement = insertStatement.Bind(result + i, "password-to3" + i);
+            var statement = insertStatement.Bind("i"+i, "password-to3" + i);
 
             // Statement ausführen
-            session.Execute(statement);
+            session.Execute(statement);  
+            
+            
         }
-        
 
+        var json = JsonConvert.SerializeObject(new
+             {
+                 isbn = "123456"+ result,
+                 title = "titel" + result,
+                 publisher = "pup" + result
+             });
+            
+             // Erstellen Sie eine CQL-Anweisung zum Einfügen der JSON in eine Tabelle
+             var cql = "INSERT INTO books JSON ?";
+             var jsonstatement = session.Prepare(cql).Bind(json);
+            
+             // Führen Sie die Anweisung aus
+             session.Execute(jsonstatement);
+        
         // Verbindung schließen
         session.Dispose();
         cluster.Dispose();
