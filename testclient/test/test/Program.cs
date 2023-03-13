7c3e33c77e208ebb8e0b34e7093a6e730199f583
Thomas Alpert <thomasalpert@gmail.com>
2023-03-13 10:10:57 +0100

    docker

--- testclient/test/test/Program.cs
+++ testclient/test/test/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        
+        var random = new Random();
+        var result = new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 4).Select(s => s[random.Next(s.Length)]).ToArray());
+        Console.WriteLine(result);
+        
         // Cassandra Cluster und Session erstellen
         // Cluster cluster = Cluster.Builder().AddContactPoint("localhost").Build();
         Cluster cluster = Cluster.Builder().AddContactPoint("193.196.55.166").Build();
@@ -13,10 +18,10 @@ class Program
         var insertStatement = session.Prepare("INSERT INTO test (name, password) VALUES (?, ?)");
 
         // 100 verschiedene Datensätze als SQL-Statements hinzufügen
-        for (int i = 1; i <= 20;  i++)
+        for (int i = 1; i <= 10;  i++)
         {
             // Parameterwerte setzen
-            var statement = insertStatement.Bind("tot" + i, "password-to3" + i);
+            var statement = insertStatement.Bind(result + i, "password-to3" + i);
 
             // Statement ausführen
             session.Execute(statement);
