7052597417bfb2971e6c4f4adb4d54792c126627
Thomas Alpert <thomasalpert@gmail.com>
2023-04-13 00:28:01 +0200

    go

--- Client/Client/Client/Program.cs
+++ Client/Client/Client/Program.cs
@@ -3,11 +3,14 @@
     internal abstract class Program
     {
         private const int ClientStartDelay = 1000;
+        private static int secondsToShutdown = 9999999;
+        
 
         public const short maxAreaX = 1000;
         public const short maxAreaY = 1000;
         public const int clientCnt = 100;
 
+        
         public static Random random = new(0); 
         public static int finishedCount = 0;
         public static object finishedCountLock = new();
@@ -18,7 +21,14 @@
 
             SimulateClients(clientCnt);
 
-            Console.ReadLine();
+            while (true)
+            {
+                Thread.Sleep(1000);
+                secondsToShutdown--;
+
+                if (secondsToShutdown == 0)
+                    break;
+            }
         }
 
         #region Private Methods
